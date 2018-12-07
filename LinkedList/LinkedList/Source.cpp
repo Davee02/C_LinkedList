6793a47ff57e8486923f77e3239567745868f6ff
David Hodel <davidhodel6@gmail.com>
2018-12-07 13:04:50 +0100

    added person-struct

--- LinkedList/LinkedList/Source.cpp
+++ LinkedList/LinkedList/Source.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+typedef struct Person
+{
+	char Firstname[40];
+	char Lastname[40];
+	int Birthyear;
+	struct Person* pNext;
+} Person;
+
 int main(int argc, char* argv[]) 
 {
+
 
 	return 0;
 }
