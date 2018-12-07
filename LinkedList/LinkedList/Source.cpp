15be0635be9b06a66cc4dbec99b4d9fd06ade0ee
David Hodel <davidhodel6@gmail.com>
2018-12-07 13:41:40 +0100

    display text-menu for execute functions

--- LinkedList/LinkedList/Source.cpp
+++ LinkedList/LinkedList/Source.cpp
@@ -2,6 +2,8 @@
 #include "stdlib.h"
 #include "time.h"
 
+void DisplayMenu();
+
 typedef struct Person
 {
 	char Firstname[40];
@@ -13,6 +15,58 @@ typedef struct Person
 int main(int argc, char* argv[]) 
 {
 	srand((unsigned)time(NULL));
+	int command = 0;
+
+	DisplayMenu();
+	while(true)
+	{
+		printf("Please the command you want to execute(0-6, -1 to quit):\n");
+		scanf_s("%d", &command);
+		if (command == -1)
+			break;
+
+		switch(command)
+		{
+		case 0:
+			DisplayMenu();
+			break;
+
+		case 1:
+			break;
+
+		case 2:
+			break;
+
+		case 3:
+			break;
+
+		case 4:
+			break;
+
+		case 5:
+			break;
+
+		case 6:
+			break;
+
+		default:
+			DisplayMenu();
+			break;
+		}
+	}
 
 	return 0;
+}
+
+void DisplayMenu()
+{
+	printf("---------- LinkedList by David H. and Yannick F. ----------\n");
+	printf("0) Display the menu\n");
+	printf("1) Create new list\n");
+	printf("2) Delete whole list\n");
+	printf("3) Delete specific item\n");
+	printf("4) Sort list\n");
+	printf("6) Output the list\n");
+	printf("-1) Quit program\n");
+	printf("------------------------------------------------------------------------\n");
 }
