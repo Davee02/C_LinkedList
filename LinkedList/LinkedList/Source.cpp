352e7575ad2c9b5b62ba98ac864094656b1461de
David <davidhodel6@gmail.com>
2018-12-16 11:55:33 +0100

    commented code

--- LinkedList/LinkedList/Source.cpp
+++ LinkedList/LinkedList/Source.cpp
@@ -23,6 +23,7 @@ void Output(Person *pHead);
 
 int main(int argc, char *argv[])
 {
+	// Initialize the pseudo-random number-generator and use the current time since midnight in milliseconds as the seed
 	srand((unsigned)time(NULL));
 	Person *pHead = NULL;
 
@@ -68,6 +69,7 @@ int main(int argc, char *argv[])
 			printf("What's the lastname of the person to be deleted? ");
 			fgets(lastName, sizeof(lastName), stdin);
 
+			// "strtok" is used to remove the newline-char "\n" from the end of the string, so that comparing doesn't fail
 			pHead = Remove(pHead, strtok(firstName, "\n"), strtok(lastName, "\n"));
 		}
 		break;
@@ -95,6 +97,7 @@ int main(int argc, char *argv[])
 }
 
 // Author: David
+// This function displays all the menu-points which can be used
 void DisplayMenu()
 {
 	printf("---------- LinkedList by David H. and Yannick F. ----------\n");
@@ -109,18 +112,23 @@ void DisplayMenu()
 }
 
 // Author: David
+// This function returns a random uppercase character
 char GetRandomCharacter()
 {
 	return GetRandomNumber('A', 'Z');
 }
 
 // Author: David
+// This function returns a random integer, which is between the under-bound and the upper-bound
+// The two bounds are inclusive
 int GetRandomNumber(int underBound, int upperBound)
 {
 	return rand() % (upperBound + 1 - underBound) + underBound;
 }
 
 // Author: David
+// This function takes a integer as a parameter, which is for the number of the to be created elements
+// This function creates a new list of people with the provided count of elements
 Person *Create(int elementCount)
 {
 	Person *pHead = NULL;
@@ -136,24 +144,39 @@ Person *Create(int elementCount)
 }
 
 // Author: David
+// This function creates a new person with a random birthyear, first- nad lastname
 Person *CreateNewPerson()
 {
 	Person *pNew = (Person *)malloc(sizeof(Person));
+
+	// The birthyear is between 1900 and 2018
 	pNew->Birthyear = GetRandomNumber(1900, 2018);
+
+	// The firstname is a random uppercase char
 	pNew->Firstname[0] = GetRandomCharacter();
+	// The string has to be terminated with '\0'
 	pNew->Firstname[1] = '\0';
+
+	// The lastname is a random uppercase char
 	pNew->Lastname[0] = GetRandomCharacter();
+	// The string has to be terminated with '\0'
 	pNew->Lastname[1] = '\0';
+
+	// The next element of the list is set to NULL
 	pNew->pNext = NULL;
 
 	return pNew;
 }
 
+// This function takes the pointer to first node of a linked-list and two strings as a parameter
+// This function deletes all peoplem from the list and frees the memory
 Person *Dispose(Person *pHead)
 {
 	return NULL;
 }
 
+// This function takes the pointer to first node of a linked-list and two strings as a parameter
+// This function removes all people with the provided first- and lastname from the list
 Person *Remove(Person *pHead, char firstName[], char lastName[])
 {
 	if(firstName == NULL || lastName == NULL)
@@ -171,11 +194,16 @@ Person *Remove(Person *pHead, char firstName[], char lastName[])
 	return pHead;
 }
 
+// This function takes the pointer to first node of a linked-list as a parameter
+// This functions sorts the whole list by the fist- and then by the lastname
 Person *Sort(Person *pHead)
 {
 	return NULL;
 }
 
+// This function takes the pointer to first node of a linked-list as a parameter
+// This function displays all people in the list.
+// Example: "Person #21: Max Muster"
 void Output(Person *pHead)
 {
 	int i = 1;
